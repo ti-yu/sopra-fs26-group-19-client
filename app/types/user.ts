023c4f599b3aaf7406cd0de3bb2b96ead5239558
ti-yu <timurtianmu.yu@uzh.ch>
2026-03-26 15:36:28 +0100

    changed user.ts to match class specifications

--- app/types/user.ts
+++ app/types/user.ts
@@ -1,7 +1,17 @@
 export interface User {
   id: string | null;
-  name: string | null;
+  isVolunteer: boolean | null;
   username: string | null;
-  token: string | null;
-  status: string | null;
+  givenName: string | null;
+  lastName: string | null;
+  password: string | null;
+  bio: string | null;
+  profilePicture: null | null;
+  address: string | null;
+  dateOfBirth: string | null;
+  gender: string | null;
+  phoneNumber: string | null;
+  emailAddress: string | null;
+  previousInserat: [string] | null;
+  reviews: [string] | null;
 }
