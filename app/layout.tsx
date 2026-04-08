93a576122befbbc7d940e73ab96e590026ab7ceb
lisgeh2 <lisa.gehrig2@uzh.ch>
2026-04-08 13:41:04 +0200

    Global Stying works (again @timur?)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { App as AntdApp, ConfigProvider, theme } from "antd";
+import { App as AntdApp, ConfigProvider } from "antd";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "@/styles/globals.css";
 
@@ -29,32 +29,40 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ConfigProvider
           theme={{
-            algorithm: theme.defaultAlgorithm,
             token: {
-              // general theme options are set in token, meaning all primary elements (button, menu, ...) will have this color
-              colorPrimary: "#22426b", // selected input field boarder will have this color as well
-              borderRadius: 8,
-              colorText: "#fff",
+              colorPrimary: "#d9737d",
+              colorBgContainer: "#f5f5f5",
+              colorText: "#000000",
+              colorBorder: "#e0e0e0",
+              colorTextPlaceholder: "#aaaaaa",
+              borderRadius: 10,
               fontSize: 16,
-
-              // Alias Token
-              colorBgContainer: "#16181D",
             },
-            // if a component type needs special styling, setting here will override default options set in token
             components: {
               Button: {
-                colorPrimary: "#75bd9d", // this will color all buttons in #75bd9d, overriding the default primaryColor #22426b set in token line 35
-                algorithm: true, // enable algorithm (redundant with line 33 but here for demo purposes)
-                controlHeight: 38,
+                colorPrimary: "#d9737d",
+                primaryColor: "#ffffff",
+                borderRadius: 30,
+                controlHeight: 55,
+                fontSize: 18,
               },
               Input: {
-                colorBorder: "gray", // color boarder selected is not overridden but instead is set by primary color in line 35
-                colorTextPlaceholder: "#888888",
-                algorithm: false, // disable algorithm (line 32)
+                colorBorder: "#e0e0e0",
+                colorTextPlaceholder: "#aaaaaa",
+              },
+              Select: {
+                colorBgContainer: "#f5f5f5",
+                colorText: "#000000",
+                colorBgElevated: "#ffffff",
+                optionSelectedBg: "#fdf0f1",
+                optionActiveBg: "#fdf0f1",
+              },
+              DatePicker: {
+                colorBgContainer: "#f5f5f5",
+                colorText: "#000000",
               },
               Form: {
-                labelColor: "#fff",
-                algorithm: theme.defaultAlgorithm, // specify a specifc algorithm instead of true/false
+                labelColor: "#000000",
               },
               Card: {},
             },
